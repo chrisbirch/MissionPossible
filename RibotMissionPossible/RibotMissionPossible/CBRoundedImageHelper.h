29982c86700e9e29dc7dece1fe1507ddadb6671b
Chris Birch <chris@ocastastudios.com>
2014-08-14 22:08:07 +0100

    Added rounded image helper

---
+++ RibotMissionPossible/RibotMissionPossible/CBRoundedImageHelper.h
@@ -0,0 +1,28 @@
+//
+//  CBRoundedImageHelper.h
+//  RibotMissionPossible
+//
+//  Created by chris on 14/08/2014.
+//  Copyright (c) 2014 Chris Birch. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+typedef void(^RoundedImageBatchDone)(NSArray* roundedImages);
+
+@interface CBRoundedImageHelper : NSObject
+
+
+
+/**
+ * Creates rounded stroked ribots
+ */
++(UIImage*)roundedImageFromImage:(UIImage*)image withOutputSize:(CGSize)size andStrokeColour:(UIColor*)colour andStrokeWidth:(CGFloat)strokeWidth;
+
+/**
+ * Creates rounded stroked ribots on a seperate thread and returns the result in a completion block
+ */
++(void)roundedImageFromImagesOnDiskWithPaths:(NSArray*)images withOutputSize:(CGSize)size andStrokeColours:(NSArray*)colours andStrokeWidth:(CGFloat)strokeWidth andCompletionBlock:(RoundedImageBatchDone)completionBlock;
+
+
+@end
---
+++ RibotMissionPossible/RibotMissionPossible/CBRoundedImageHelper.m
@@ -0,0 +1,73 @@
+//
+//  CBRoundedImageHelper.m
+//  RibotMissionPossible
+//
+//  Created by chris on 14/08/2014.
+//  Copyright (c) 2014 Chris Birch. All rights reserved.
+//
+
+#import "CBRoundedImageHelper.h"
+
+@implementation CBRoundedImageHelper
+
+
++(UIImage*)roundedImageFromImage:(UIImage*)image withOutputSize:(CGSize)size andStrokeColour:(UIColor*)colour andStrokeWidth:(CGFloat)strokeWidth
+{
+    UIGraphicsBeginImageContext(size);
+    CGContextRef context = UIGraphicsGetCurrentContext();
+    CGRect cRect =CGRectMake(0, 0, size.width, size.height);
+    
+    //Create the circle
+    CGContextAddEllipseInRect(context, cRect);
+    //And use it for the clipping
+    CGContextClip(context);
+    //clip the context
+    CGContextClearRect(context, cRect);
+    
+    //Now draw the image in the clipping rect
+    [image drawInRect:CGRectMake(cRect.origin.x, cRect.origin.y, cRect.size.width, cRect.size.height)];
+    
+    CGContextSetLineWidth(context, 5);
+    CGContextStrokeEllipseInRect(context, cRect);
+    
+    UIImage* output = UIGraphicsGetImageFromCurrentImageContext();
+    
+    UIGraphicsEndImageContext();
+    
+    return output;
+    
+}
+
++(void)roundedImagesOnDiskWithPaths:(NSArray *)images withOutputSize:(CGSize)size andStrokeColours:(NSArray *)colours andStrokeWidth:(CGFloat)strokeWidth andCompletionBlock:(RoundedImageBatchDone)completionBlock
+{
+    
+    //Start an activity indicator here
+    
+    dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
+
+        //Round the images on the background thread
+        
+        NSMutableArray* roundedImages = [NSMutableArray new];
+        NSUInteger indexOfImage=0;
+        
+        for (NSString* imagePath in images)
+        {
+            UIImage* image = [UIImage imageWithContentsOfFile:imagePath];
+            UIColor* thisColour = colours[indexOfImage++];
+            UIImage* rounded = [CBRoundedImageHelper roundedImageFromImage:image withOutputSize:size andStrokeColour:thisColour andStrokeWidth:strokeWidth];
+            
+            if (rounded)
+            {
+                [roundedImages addObject:rounded];
+            }
+        }
+        
+        dispatch_async(dispatch_get_main_queue(), ^(void) {
+            
+            //Alert code we've finished rounding the images
+            completionBlock(roundedImages);
+            
+        });
+    });
+}
+@end
